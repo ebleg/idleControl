d091318bfb94ede61026e0256434371c90527507
Emiel Legrand <emiel.legrand@gmail.com>
2017-11-17 23:27:42 +0100

    intake manifold identification

--- fun/identify_intakemanifold.m
+++ fun/identify_intakemanifold.m
@@ -1,27 +1,21 @@
 function [V_m] = identify_intakemanifold(dataFile, pars, plot_validation_toggle)
-%IDENTIFY volume of the INTAKE MANIFOLD
+    %IDENTIFY volume of the INTAKE MANIFOLD
 
-%% load data
-data = load(dataFile);
-meas = data.meas;
+    %% load data
+    data = load(dataFile);
+    meas = data.meas;
+    model = 'id_manifold_model.slx';
 
-%% set fminserach options 
-efun_fminsearch = @(V_m) model_error(V_m, meas);
-setopt = optimset('Algorithm','sqp','display','iter','Maxit',30);
+    %% set fminserach options 
+    mod = @(V_m) model_error(model, V_m, meas);
+    setopt = optimset('Algorithm','sqp','display','iter','Maxit',30);
 
-%% call fminsearch
-V_m = fminsearch(efun_fminsearch, V_mini, setopt);
+    %% call fminsearch
+    V_m = fminsearch(efun_fminsearch, V_mini, setopt);
 
-%% validate
-if plot_validation_toggle
-    % plot stuff
-end
+    %% validate
+    if plot_validation_toggle
+        % plot stuff
+    end
      
 end
-
-function [error] = model_error(V_m , meas)
-%% error function for V_m
-
-
-end
-
