2ae8c967fde2c3effd52bfcd05b1ea0f1c2a3afd
Emiel Legrand <emiel.legrand@gmail.com>
2017-11-17 13:25:10 +0100

    Wrote identify_throttle function, added parameters to generic structi

--- main.m
+++ main.m
@@ -12,22 +12,45 @@ addpath(genpath('.'));
 %% MAIN TOGGLE OPTIONS
 identify_params = 1; % general switch
 
-identify_throttle = 1;
-identify_engine = 1;
-identify_manifold = 1;
-identify_generator = 1;
+identify_throttle_toggle = 1;
+identify_engine_toggle = 1;
+identify_manifold_toggle = 1;
+identify_generator_toggle = 1;
 
-plot_data = 1;
-convert_data = 0;
+plot_validation = 1;
+convert_data = 1;
+
+%% LOAD PARAMETERS
+fprintf('------ MAIN -------\n \n');
+fprintf('$ Loading parameters ...');
+run params;
+run parinit;
+fprintf(' Done\n');
 
 %% CODE
 % Data conversion
 if convert_data
-  convertData();
-  disp('Data converted')
+  fprintf('$ Converting data ...');
+  convertData('dynamic_0006_extracted.mat');
+  fprintf(' Done\n');
 end
 
 % Parameter identification
+if identify_params
+    if identify_throttle_toggle
+        fprintf('\n$ Identify throttle parameters ...'); 
+        [alpha0, alpha1] = identify_throttle('quasistatic_0007_extracted.mat', pars, plot_validation);
+        pars.id.alpha0 = alpha0;
+        pars.id.alpha1 = alpha1;
+        fprintf(' Done\n');
+    end
 
+end
+
+%% LOAD IDENTIFIED PARAMETERS
+%run parid;
+
+
+fprintf('\n------ END --------\n');
 
 
---
+++ par/parinit.m
@@ -0,0 +1,1 @@
+%% INTIIAL VALUES
