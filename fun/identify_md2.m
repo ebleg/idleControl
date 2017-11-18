0cfae818e65fe582292604b3388bc40d4cae16cf
Emiel Legrand <emiel.legrand@gmail.com>
2017-11-18 17:13:30 +0100

    created identify_md2 using the new model for the intake manifold volume verification

--- fun/identify_md2.m
+++ fun/identify_md2.m
@@ -1,32 +1,29 @@
-function [V_m] = identify_intakemfd(dataFile, pars, plot_validation_toggle)
-%% IDENTIFY VOLUME OF THE INTAKE MANIFOLD
-    %% load data
+function [V_m] = identify_md2(dataFile, pars, plot_validation_toggle)
+%% IDENTIFY INTAKE MANIFOLD VOLUME [2]S
+% -------------------------------------------------------------------------
+% Identify intake manifold volume V_m by using the reduced model
+% id_manifol 2.
+% -------------------------------------------------------------------------
+
+%% Load Data
     data = load(dataFile);
     meas = data.meas;
-    model_file = 'id_manifold_model.slx';
-
-%% set fminsearch function
-    efun_fminsearch = @(V_m) model_error(V_m, meas, pars, model_file, plot_validation_toggle);
-
-%% call fminsearch
+    model_file = 'id_manifold_2.slx';
+    
+%% Define function handle
+    efun_fminsearch = @(V_m) model_error_intakemfd2(V_m, meas, pars, model_file, plot_validation_toggle);
+    
+%% Minimization
     V_m = fminsearch(efun_fminsearch, pars.init.V_m, pars.fmin_opt);
 
-    %% validate
-    if plot_validation_toggle
-        % plot stuff
-    end
-
 end
-% flag 1
 
-function [error] = model_error(V_m , meas, pars, model_file, plot_validation_toggle)
-    %% error function for V_m
+function [error] = model_error_intakemfd2(V_m, meas, pars, model_file, plot_validation_toggle)
+%% COMPUTE RMS ERROR BETWEEN MODEL AND MEASUREMENTS
     pars.id.V_m = V_m;
     [~, ~, p_m_model] = sim(model_file,... 
                             [meas.time(1) meas.time(end)],...
-                            pars.sim_opt,...
-                            [meas.time, meas.u_alpha.signals.values, meas.omega_e.signals.values]); %% add other inputs
-
+                            pars.sim_opt); %% add other inputs
     error = sum((meas.p_m.signals.values./(10^5) - p_m_model./(10^5)).^2);
 
     if plot_validation_toggle
