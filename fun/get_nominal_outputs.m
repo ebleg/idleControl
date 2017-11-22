d8138c7551be32e141e2fb090d6df595714d961d
Emiel Legrand <emiel.legrand@gmail.com>
2017-11-22 20:12:30 +0100

    cleaning up

--- fun/get_nominal_outputs.m
+++ fun/get_nominal_outputs.m
@@ -1,14 +1,17 @@
 function [w_e_nom, delay1, delay2] = get_nominal_outputs(pars)
 %% RUN STEADY STATE SIMULATION TO GET NORMALIZED REFERENCES
+    % Load parameters from pars struct
     u_alpha_input = pars.nom.u_alpha;
     du_ign_input = pars.nom.du_ign;
+
+    % Model name
     model_name = 'full_model_steady_state.slx';
     
+    % Generate step inputs
     time = 0:0.1:10;
     step_u_alpha = u_alpha_input*double(time>=1)';
     step_du_ign = du_ign_input*double(time>=1)';
     
-    disp('Begin sim!');
     % generate step signal
     [~, ~, w_e] = sim(model_name, ...
                       [time(1) time(end)], ...
@@ -16,8 +19,6 @@ function [w_e_nom, delay1, delay2] = get_nominal_outputs(pars)
                       [time', ...
                       step_u_alpha, ...
                       step_du_ign]);
-                      
-    disp('Sim done!');
     
     w_e_nom = mean(w_e((end-100):end));
     
