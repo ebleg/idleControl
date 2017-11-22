5816449aafc47ec3a85b02820ed879e53a08f58a
Emiel Legrand <emiel.legrand@gmail.com>
2017-11-22 19:43:15 +0100

    akja

--- main.m
+++ main.m
@@ -7,139 +7,156 @@
 % Master file
 % -------------------------------------------------------------------------
 
-%% PREPARE WORKSPACE
-clear;
-clc;
-close all;
+disp('main ...');
 
-addpath(genpath('.'));
+%% PREPARE WORKSPACE
+    clear;
+    clc;
+    close all;
+
+    addpath(genpath('.'));
+% -------------------------------------------------------------------------
 
 %% TOGGLE OPTIONS
-identify_params = 0; % general switch
-linearize_model = 1;
-controller_design = 0;
+    % Global toggles
+    identify_params = 0; % general switch
+    linearize_model = 1;
+    controller_design = 0;
 
-identify_throttle_toggle = 1;
-identify_engine_toggle = 1;
-identify_manifold_toggle = 1;
-identify_manifold2_toggle = 0;
-identify_inertia_toggle = 0;
+    % Specific toggles
+    identify_throttle_toggle = 1;
+    identify_engine_toggle = 1;
+    identify_manifold_toggle = 1;
+    identify_manifold2_toggle = 0;
+    identify_inertia_toggle = 0;
 
-plot_validation_toggle = 1;
-convert_data_toggle = 0;
+    % Additional toggles
+    plot_validation_toggle = 1;
+    convert_data_toggle = 0;
 
-validation_toggle = 0;
+    validation_toggle = 0;
+% -------------------------------------------------------------------------
 
 %% LOAD PARAMETERS
-fprintf('------ MAIN -------\n \n');
-fprintf('$ Loading static parameters ...');
-run params;
+    fprintf('$ Loading static parameters ...');
+    run params;
 
-%% create filename variables
-dataFile_id_q = 'quasistatic_0007_extracted.mat';
-dataFile_id_dyn = 'dynamic_0028_extracted.mat';
-dataFile_val = 'dynamic_0005_extracted.mat';
+    % Create filename variables
+    dataFile_id_q = 'quasistatic_0007_extracted.mat';
+    dataFile_id_dyn = 'dynamic_0028_extracted.mat';
+    dataFile_val = 'dynamic_0005_extracted.mat';
 
-fprintf(' Done\n');
-
-%% CODE
-%% Data conversion
-if convert_data_toggle
-    fprintf('$ Converting data ...');
-    convert_data('dynamic_0006_extracted.mat');
     fprintf(' Done\n');
-end
+% -------------------------------------------------------------------------
 
-%% Parameter identification
-if identify_params
-    
-    if identify_throttle_toggle
-        fprintf('\n$ Identify throttle parameters ...');
-        pars = parinit(dataFile_id_q, pars);
-        [pars.id.alpha_0, pars.id.alpha_1] = ...
-            identify_throttle(dataFile_id_q, ...
-            pars, ...
-            plot_validation_toggle);
+%% DATA CONVERSION
+    if convert_data_toggle
+        fprintf('$ Converting data ...');
+        convert_data('dynamic_0006_extracted.mat');
         fprintf(' Done\n');
     end
-    
-    if identify_engine_toggle
-        fprintf('$ Identify engine parameters ...');
-        pars = parinit(dataFile_id_q, pars);
-        [pars.id.gamma_0, pars.id.gamma_1] = ...
-            identify_engine(dataFile_id_q, ...
-            pars, ...
-            plot_validation_toggle);
-        fprintf(' Done\n');
+% -------------------------------------------------------------------------
+
+%% PARAMETER IDENTIFICATION
+    if identify_params
+        % Identify throttle parameters alpha_0, alpha_1
+        if identify_throttle_toggle
+            fprintf('\n$ Identify throttle parameters ...');
+            pars = parinit(dataFile_id_q, pars);
+            [pars.id.alpha_0, pars.id.alpha_1] = ...
+                identify_throttle(dataFile_id_q, ...
+                pars, ...
+                plot_validation_toggle);
+            fprintf(' Done\n');
+        end
+        
+        % Identify engine parameters gamma_0, gamma_1
+        if identify_engine_toggle
+            fprintf('$ Identify engine parameters ...');
+            pars = parinit(dataFile_id_q, pars);
+            [pars.id.gamma_0, pars.id.gamma_1] = ...
+                identify_engine(dataFile_id_q, ...
+                pars, ...
+                plot_validation_toggle);
+            fprintf(' Done\n');
+        end
+        
+        % Identify intake manifold volume V_m (second method)
+        if identify_manifold2_toggle
+            fprintf('$ Identify intake manifold volume (model 2) ...\n');
+            [pars.id.V_m] = ...
+                identify_md2(dataFile_id_dyn, ...
+                pars, ...
+                plot_validation_toggle);
+        end
+        
+        % Identify intake manifold volume V_m (first method)
+        if identify_manifold_toggle
+            fprintf('$ Identify intake manifold volume (model 1) ...\n');
+            pars = parinit(dataFile_id_dyn, pars);
+            [pars.id.V_m] = ...
+                identify_intakemfd(dataFile_id_dyn, ...
+                pars, ...
+                plot_validation_toggle);
+            fprintf('... Done\n');
+        end
+        
+        if identify_inertia_toggle
+            fprintf('$ Identify torque and inertia ...');
+            pars = parinit(dataFile_id_dyn, pars);
+            [pars.id.eta_0, ...
+                pars.id.eta_1, ...
+                pars.id.beta_0, ...
+                pars.id.THETA_e] = ...
+                identify_ti(dataFile_id_dyn, ...
+                pars, ...
+                plot_validation_toggle);
+            fprintf(' Done\n');
+        end
+    else
+        % Load parameters that are identified before
+        fprintf($'Loading stored parameters ...');
+        load('pars.mat');
+        fprintf('$ Done\n');
     end
-    
-    if identify_manifold2_toggle
-        fprintf('$ Identify intake manifold volume (model 2) ...\n');
-        [pars.id.V_m] = ...
-            identify_md2(dataFile_id_dyn, ...
-            pars, ...
-            plot_validation_toggle);
+% -------------------------------------------------------------------------
+
+%% VALIDATE MODEL
+    % Validate identified parameters with engine speed plot
+    if validation_toggle
+        fprintf('$ Validating model ... ');
+        pars = parinit(dataFile_val, pars);
+        validation(dataFile_val, pars)
+        fprintf('Done\n');
     end
-    
-    if identify_manifold_toggle
-        fprintf('$ Identify intake manifold volume (model 1) ...\n');
-        pars = parinit(dataFile_id_dyn, pars);
-        [pars.id.V_m] = ...
-            identify_intakemfd(dataFile_id_dyn, ...
-            pars, ...
-            plot_validation_toggle);
-        fprintf('... Done\n');
+% -------------------------------------------------------------------------
+
+%% NORMALIZE AND LINEARIZE THE MODEL
+    if linearize_model
+        % Compute the nominal input values for the steady-state model run
+        fprintf('$ Computing nominal inputs ... ');
+        [pars.nom.u_alpha, pars.nom.du_ign] = get_nominal_inputs('dynamic_0005_extracted.mat');
+        fprintf('Done\n');
+        
+        % Run the steady state model and extracting delays and engine speed
+        fprintf('$ Computing nominal outputs and delays ... ');
+        [pars.nom.w_e, pars.nom.delay1, pars.nom.delay2] = get_normalized_outputs(pars);
+        fprintf('Done\n');
+
+        % Linearize normalized model
+        fprintf('$ Computing linearization ... ');
+        system = linearize(pars);
+        fprintf('Done\n');
+        
+    else % in case the model is already linearized
+        load('system.mat');
     end
-    
-    if identify_inertia_toggle
-        fprintf('$ Identify torque and inertia ...');
-        pars = parinit(dataFile_id_dyn, pars);
-        [pars.id.eta_0, ...
-            pars.id.eta_1, ...
-            pars.id.beta_0, ...
-            pars.id.THETA_e] = ...
-            identify_ti(dataFile_id_dyn, ...
-            pars, ...
-            plot_validation_toggle);
-        fprintf(' Done\n');
+
+    if controller_design
+        pars = control_design(system, pars)
+    else
+        load('controller.mat')
     end
-else
-    %% LOAD IDENTIFIED PARAMETERS
-    load('pars.mat');
-end
+% -------------------------------------------------------------------------
 
-%% Validate model
-if validation_toggle
-    fprintf('$ Validating model ... ');
-    pars = parinit(dataFile_val, pars);
-    validation(dataFile_val, pars)
-    fprintf('Done\n');
-end
-
-%% Normalize and linearize the model
-if linearize_model
-    fprintf('$ Computing nominal inputs ... ');
-    [pars.nom.u_alpha, pars.nom.du_ign] = get_nominal_inputs('dynamic_0005_extracted.mat');
-    fprintf('Done\n');
-    fprintf('u_alpha_nomin: %f\n', pars.nom.u_alpha);
-    fprintf('du_ign_nomin: %f', pars.nom.du_ign);
-    
-    fprintf('$ Computing nominal outputs and delays ... ');
-    [pars.nom.w_e, pars.nom.delay1, pars.nom.delay2] = get_normalized_outputs(pars);
-    fprintf('Done\n');
-
-    fprintf('$ Computing linearization ... ');
-    system = linearize(pars);
-    fprintf('Done\n');
-else
-    load('system.mat');
-end
-
-if controller_design
-    pars = control_design(system, pars)
-else
-    load('controller.mat')
-end
-
-fprintf('\n------ END --------\n');
-
+disp('$ End of main reached');
