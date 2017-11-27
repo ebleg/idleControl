02ee9cc8b0375e99b9c09739b6ec6555b7f8d97d
Emiel Legrand <emiel.legrand@gmail.com>
2017-11-27 19:09:55 +0100

    fixed one bug, now there is an error when converting from tf to ss

--- fun/create_observer.m
+++ fun/create_observer.m
@@ -20,15 +20,16 @@ function [observer] = create_observer(ss,pars)
         x_hat_dot = icsignal(11); % intermediate signal; time-derivative of x_hat
     
         % Connect observer blocks and signals
-        M  = iconnect; 
+        M = iconnect; 
         M.Input = [u_nl_in; y_nl_in]; 
         M.Output = [x_hat; y_hat]; 
         % Equation 1: everything before integrator
-        M.Equation{1} = equate(x_hat_dot, u_nl_in*B + x_hat*A + (y_nl_in - y_hat)*L);
+        M.Equation{1} = equate(x_hat_dot, B*u_nl_in + A*x_hat + L*(y_nl_in - y_hat));
         % Equation 2: integrate time derivatives of states
         M.Equation{2} = equate(x_hat, s*x_hat_dot);
         % Equation 3: multiply with C to get output from the states
         M.Equation{3} = equate(y_hat, C*x_hat);
-    
+        observer_tf = minreal(tf(M.System));
+        observer = ss(observer_tf);
         % Create state-space representation for the observer system
-        observer = ss(M.System);
+        
