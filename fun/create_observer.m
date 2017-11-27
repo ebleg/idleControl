9827faded497e0746796982ee7f22751fdee3c2c
Emiel Legrand <emiel.legrand@gmail.com>
2017-11-27 18:44:53 +0100

    tried to fix some bugs

--- fun/create_observer.m
+++ fun/create_observer.m
@@ -15,16 +15,16 @@ function [observer] = create_observer(ss,pars)
         % Create signals for iconnect
         y_nl_in = icsignal(1); % normalized output signal from nonlinear plant
         u_nl_in = icsignal(2); % normalized input signal for nonlinear plant
-        x_hat = icsignal(12); % states as predicted by the observer
-        y_hat = icsignal(1) % output as predicted by the observer
+        x_hat = icsignal(11); % states as predicted by the observer
+        y_hat = icsignal(1); % output as predicted by the observer
         x_hat_dot = icsignal(11); % intermediate signal; time-derivative of x_hat
     
         % Connect observer blocks and signals
-        M  = iconnect(); 
+        M  = iconnect; 
         M.Input = [u_nl_in; y_nl_in]; 
         M.Output = [x_hat; y_hat]; 
         % Equation 1: everything before integrator
-        M.Equation{1} = equate(x_hat_dot, B*u_nl_in + A*x_hat + L*(y_nl_in - y_hat));
+        M.Equation{1} = equate(x_hat_dot, u_nl_in*B + x_hat*A + (y_nl_in - y_hat)*L);
         % Equation 2: integrate time derivatives of states
         M.Equation{2} = equate(x_hat, s*x_hat_dot);
         % Equation 3: multiply with C to get output from the states
