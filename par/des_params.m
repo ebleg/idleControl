0b7517a4fb65e42f4671b63cc883600cf5fd3ecf
Twan van der Sijs <vtwan@student.ethz.ch>
2017-11-28 18:56:23 +0100

    tralalallala

--- par/des_params.m
+++ par/des_params.m
@@ -1,10 +1,10 @@
 %% TUNING PARAMETERS INTIALIZATION
 
-pars.des.K_l = 10;
+pars.des.K_l = 20; % higher is slower?? not sure
 
-r1 = 0.05;
-r2 = 0.01; 
+r1 = 0.4; % less is faster
+r2 = 0.015; % less is faster 
 pars.des.R_ctrl = [r1 0; 0 r2];
 clear r1 r2
 
-pars.des.q = 10;
+pars.des.q = 15; % less is faster (observer, should be as slow as possible)
