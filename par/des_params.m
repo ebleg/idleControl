9988aacd7ed9c600fd38644cec8baa6ceec2fc8c
Emiel Legrand <emiel.legrand@gmail.com>
2017-12-12 14:56:07 +0100

    new controllers that don't work

--- par/des_params.m
+++ par/des_params.m
@@ -1,10 +1,10 @@
 %% TUNING PARAMETERS INTIALIZATION
 
-pars.des.K_l = 45; % higher is slower?? not sure
+pars.des.K_l = 10; % higher is slower?? not sure
 
-r1 = 0.2; % less is faster
-r2 = 0.001; % less is faster 
+r1 = 7; % less is faster
+r2 = 0.008; % less is faster 
 pars.des.R_ctrl = [r1 0; 0 r2];
 clear r1 r2
 
-pars.des.q = 10; 
+pars.des.q = 3; 
